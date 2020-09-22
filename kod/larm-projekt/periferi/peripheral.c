5bd15ae5330ff17757db97549c2303117d2bfd3f
Viktor Frideen <frideen@net.chalmers.se>
2020-09-22 21:03:41 +0200

    update

--- kod/larm-projekt/periferi/peripheral.c
+++ kod/larm-projekt/periferi/peripheral.c
@@ -11,7 +11,7 @@ void think() {
 	
 	while(1) {
 		//DUMP_numeric(read_door_sensor());
-		DUMP_numeric(proximity_read());
+		//DUMP_numeric(proximity_read());
 		
 		// Delay 10^6 us = 1s
 		delay(1000000);
