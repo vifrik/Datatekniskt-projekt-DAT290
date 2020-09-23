12b176e36f7b2e62000ebace2d0f081eef62a086
Viktor Frideen <frideen@student.chalmers.se>
2020-09-23 17:30:33 +0200

    can messages

---
+++ kod/larm-projekt/kommunikation/door_example.c
@@ -0,0 +1,111 @@
+#include "messages.h"
+
+typedef struct {
+    uchar active;
+    uchar id;
+    uchar alarm;
+    uchar tolerance;
+} State;
+
+State state;
+
+void state_init(void) {
+    state.active = 0;
+    state.id = 0;
+    state.alarm = 0;
+    state.tolerance = 0;
+}
+
+void alarm_raise() {
+    state.alarm = 1;
+
+    CANMsg msg;
+
+    msg.msgId = ALARM;
+    msg.nodeId = state.id;
+    msg.length = 0;
+    msg.buff = 0;
+
+    can_send(&msg);
+}
+
+void alarm_lower() {
+    state.alarm = 0;
+}
+
+void poll_respond(CANMsg *msg) {
+    CANMsg response;
+    response.msgId = POLL_RESPONSE;
+    response.nodeId = state.id;
+    response.length = 2;
+    response.buff = 0;
+    response.buff = ~msg->buff;
+
+    can_send(&response);
+}
+
+void request_id() {
+    CANMsg msg;
+    msg.msgId = DICP_REQUEST;
+    msg.nodeId = state.id;
+    msg.length = 0;
+    msg.buff = 0;
+
+    can_send(&msg);
+}
+
+void update_door_id(CANMsg *msg) {
+    state.id = msg->data[0];
+}
+
+void update_tolerance(CANMsg *msg) {
+    state.tolerance = msg->data[0];
+}
+
+void receiver(void) {
+	DUMP("CAN message received: ");
+	
+    CANMsg msg;
+    can_receive(&msg)
+	
+    if (!state.id || msg.msgId == state.id) {
+        switch(msg.msgId) {
+            case ALARM:
+                break;
+            case ALARM_OFF:
+                alarm_lower();
+                break;
+            case POLL_REQUEST:
+                break;
+            case POLL_RESPONSE:
+                poll_respond(&msg);
+                break;
+            case DICP_REQUEST:
+                break;
+            case DICP_RESPONSE:
+                update_door_id(&msg);
+                break;
+            case TOL_SET:
+                update_tolerance(&msg);
+                break;
+            case ACTIVE_ON:
+                state.active = 1;
+                break;
+            case ACTIVE_OFF:
+                state.active = 0;
+                break;
+        }
+    }
+}
+
+void init(void) {
+    state_init();
+    can_init1(reciever);
+
+    request_id();
+}
+
+void think() {
+    // Read door sensor here
+    // If 1 (open), alarm_raise();
+}
--- kod/larm-projekt/kommunikation/messages.h
+++ kod/larm-projekt/kommunikation/messages.h
@@ -1,12 +1,78 @@
 #ifndef MESSAGES_H
 #define MESSAGES_H
 
+/*
+
+// Triggers an alarm when recieved on the central device
+ALARM:
+msgId = ALARM
+nodeId = sender id
+dir = 0
+
+// Disables an alarm on a peripheral device
+ALARM_OFF:
+msgId = ALARM_OFF
+nodeId = reciever id
+dir = 1
+
+// Perform a poll reqeust from a peripheral device with id nodeId
+POLL_REQUEST:
+msgId = POLL_REQUEST
+nodeId = reciever id
+dir = 0
+length = 2
+data = arbitrary sequency of bits of length 16
+
+// Respond to poll request
+POLL_RESPONSE:
+msgId = POLL_RESPONSE
+nodeId = sender id
+dir = 1
+length = 2
+data = bitwise complement of request data 
+
+// Dynamic ID Configuration Protocol request, request an ID from the central device
+DICP_REQUEST:
+msgId = DICP_REQUEST
+nodeId = 0
+dir = 0
+
+// Respond to request with ID
+DICP_RESPONSE:
+msgId = DICP_RESPONSE
+nodeId = 0xF
+dir = 1
+lenght = 1
+data = id
+
+// Set tolerance in peripheral device with id nodeId
+TOL_SET:
+msgId = TOL_SET
+nodeId = reciever id
+dir = 1
+length = 1
+data = tolerance
+
+// Activate a periferal device
+ACTIVE_ON:
+msgId = ACTIVE_ON
+nodeId = reciever id
+dir = 1
+
+// Disable a peripheral device
+ACTIVE_OFF:
+msgId = ACTIVE_OFF
+nodeId = reciever id
+dir = 1
+
+*/
+
 enum MSG_ID {
 	ALARM,
 	ALARM_OFF,
 	POLL_REQUEST,
 	POLL_RESPONSE,
-	DICP_REQUEST, // Dynamic ID Configuration Protocol
+	DICP_REQUEST,
 	DICP_RESPONSE,
 	TOL_SET,
 	ACTIVE_ON,
