d2fe6357ceb6c4ecbec611a9be8ab37c2fa9f28f
Viktor Frideen <frideen@student.chalmers.se>
2020-10-17 15:56:46 +0200

    Updated interference device

--- kod/larm-projekt/interference/interference.c
+++ kod/larm-projekt/interference/interference.c
@@ -1,84 +1,64 @@
 #include "interference.h"
 #include "can.h"
 #include "usart.h"
-#include "shared_peripheral.h"
 #include "messages.h"
 #include "stk.h"
 
-void interference_init(void) {
-	state_init();
-	//state.id = 1;
-	stk_init();
-	can1_init(interference_receiver());
+// Default 1s delay
+unsigned int delay_time = 10000;
+CANMsg msg;
+
+void append_char_buff(unsigned char buffer[], unsigned char length, unsigned char c) {
+	for (int i = 0; i < length - 1; i++) {
+		buffer[i] = buffer[i+1];
+	}
+
+	buffer[length-1] = c;
+
+	_outchar(c);
+}
+
+void clear_buff(unsigned char buffer[], unsigned char length) {
+	for (int i = 0; i < length; i++) {
+		buffer[i] = 48;
+	}
 }
 
 void interference_receiver(void) {
 	DUMP("CAN message received: ");
 }
 
-//Läser in en sträng från USART tills det kommer en ny rad.
-//Tar in det första avlästa värdet från konsolen och adressen till var strängen skall ligga.
-//Returnerar längden på den inlästa strängen
-uchar get_string(uchar c, uchar *address) {
-	uchar *s;
-	s = address;
-	uchar input = c;
-	uchar length = 0;
-	
-	while(input != '\n') {
-		*s = input;
-		s++;
-		length++;
-		_outchar(input);
-		input = _getchar();
-	}
-	*s = '\0';
-	_outchar('\n');
-	
-	return length;
+void timer_callback() {
+	can_send(&msg);
+	DUMP("Sent");
+	delay_no_block(delay_time);
 }
 
-uchar delayDone;
-
-void delay_done(void) {
-	delayDone = 0;
-}
-
-void clear_string(uchar *address, uchar length) {
-	uchar *iAddress;
-	iAddress = address;
-	for(uchar i = 0; i < length; i++) {
-		*iAddress++ = '\0';
-	}
+void interference_init(void) {
+	can1_init(interference_receiver);
+	
+	msg.nodeId = 0;
+	msg.msgId = POLL_RESPONSE;
+	msg.dir = TO_CENTRAL;
+	msg.length = 0;
+	clear_buff(msg.buff, 8);
+	
+	stk_init();
+	callback_init(timer_callback);
+	timer_callback();
 }
 
 void interference_think(void) {
-	CANMsg *msg;
-	canmsg_init(msg);
-	msg->msgId = POLL_RESPONSE;
-	callback_init(delay_done);
-	
-	uchar *s;
-	uchar *sAddress;
-	sAddress = s;
-	uchar length = 0;
-	uchar c;
-	unsigned int usDelay = 1000000;
+	unsigned char charBuffer[10] = {48,48,48,48,48,48,48,48,48,48};
+
+	// Main loop
 	while(1) {
-		DUMP_numeric(usDelay);
-		//can_send(msg);
-		delayDone = 1;
-		delay_no_block(usDelay);
-		while(delayDone) {
-			c = _tstchar();
-			if(c) {
-				//*sAddress = '\0';
-				//clear_string(sAddress, length);
-				length = get_string(c, sAddress);
-				s = sAddress;
-				usDelay = atoi(s);
-				break;
-			}
-		}
+		unsigned char c = _tstchar();
+		if (c == 13 || c == 10) {
+			usart_send("Updated delay to: ");
+			for (int i = 0; i<10;i++) _outchar(charBuffer[i]);
+			usart_send_numericl(delay_time = atoi(charBuffer));
+			clear_buff(charBuffer,10);
+		} else if (c) append_char_buff(charBuffer, 10, c);
 	}
 }
